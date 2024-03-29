f21b6003325fc89c0052276bc3dd409737fd12e1
Rajat Gorakhpuriya <rajat.gorakhpuriya@gmail.com>
2024-03-29 08:01:56 +0530

    code bundeling for lazy loading

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestauroMenu from "./components/RestaurentMenu";
+import Shimmer from "./components/Shimmer";
+// import Grocery from "./components/Grocery";
 
-
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
     return (
@@ -31,11 +34,15 @@ const appRouter = createBrowserRouter(
                 },
                 {
                     path: "/about",
-                    element: <About />
+                    element: <Suspense fallback={<Shimmer/>}><About /></Suspense>
                 },
                 {
                     path: "/contact",
                     element: <Contact />
+                }, 
+                {
+                    path: "/grocery",
+                    element: <Suspense fallback={<h1>Loading</h1>}><Grocery /></Suspense>
                 },
                 {
                     path: "/restaurent/:resId",
